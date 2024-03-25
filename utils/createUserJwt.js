b7c4e973359bc80513ae28357ee653582c930ef8
Matiur847 <mclaren847329@gmail.com>
2024-03-25 22:41:24 +0600

    modified cookie

--- utils/createUserJwt.js
+++ utils/createUserJwt.js
@@ -1,26 +1,28 @@
-const { jwtSec, jwtExp } = require("../config/config")
-const User = require("../models/userModel")
-const jwt = require('jsonwebtoken')
+const { jwtSec, jwtExp } = require("../config/config");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
-// create jwtToken or save is cookie 
-const jwtUser =  (user, statusCode, res) => {
-    const token = jwt.sign({id: user._id}, jwtSec, {
-        expiresIn: jwtExp
-    })
+// create jwtToken or save is cookie
+const jwtUser = (user, statusCode, res) => {
+  const token = jwt.sign({ id: user._id }, jwtSec, {
+    expiresIn: jwtExp,
+  });
 
-    // create cookie option 
-    const options = {
-        expires: new Date(
-            Date.now() + 4 * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true
-    }
+  console.log("token", token);
 
-    res.status(statusCode).cookie('token', token, options).json({
-        success: true,
-        user,
-        token
-    })
-}
+  // create cookie option
+  const options = {
+    // expires: new Date(Date.now() + 4 * 24 * 60 * 60 * 1000),
+    expires: new Date(new Date().getTime() + 15 * 60 * 1000),
+    maxAge: 900000,
+    httpOnly: true,
+  };
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    user,
+    token,
+  });
+};
 
 module.exports = jwtUser;
