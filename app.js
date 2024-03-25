195f2f6cabce6ca483b68d663f944693c1376013
Matiur847 <mclaren847329@gmail.com>
2024-03-25 22:14:00 +0600

    new headers for cookie

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.use((req, res, next) => {
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 });
 
