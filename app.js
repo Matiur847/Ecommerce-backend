ad68e9af2f7c642d05bce9c3f9283fce65aca337
Matiur847 <107800262+Matiur847@users.noreply.github.com>
2024-03-27 02:32:00 +0600

    added cors modified

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://ecommerce-backend-tawny-nine.vercel.app",
+    ],
     credentials: true,
   })
 );
