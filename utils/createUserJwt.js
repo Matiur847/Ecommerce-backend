38edfb104ea494eafb25cafa2fc7d19c7806a4db
Matiur847 <mclaren847329@gmail.com>
2024-03-27 00:09:34 +0600

    samesite per

--- utils/createUserJwt.js
+++ utils/createUserJwt.js
@@ -4,16 +4,23 @@ const jwt = require("jsonwebtoken");
 
 // create jwtToken or save is cookie
 const jwtUser = (user, statusCode, res) => {
-  const token = jwt.sign({ id: user._id }, jwtSec);
+  const token = jwt.sign({ id: user._id }, jwtSec, {
+    expiresIn: jwtExp,
+  });
 
   // create cookie option
   const options = {
     expires: new Date(Date.now() + 4 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: true,
+    samesite: "none",
   };
 
-  res.cookie("token", token);
+  res.status(statusCode).cookie("token", token).json({
+    success: true,
+    user,
+    token,
+  });
 };
 
 module.exports = jwtUser;
