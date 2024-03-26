a56dbe90192da79b0659fa544ef8ea6f7c068808
Matiur847 <mclaren847329@gmail.com>
2024-03-26 17:46:36 +0600

    addes cors modified

--- app.js
+++ app.js
@@ -14,13 +14,7 @@ const cookieParser = require("cookie-parser");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-    exposedHeaders: ["set-cookie"],
-  })
-);
+app.use(cors());
 
 app.use(cookieParser());
 app.use(fileUpload());
@@ -34,7 +28,7 @@ app.use((req, res, next) => {
   // Website you wish to allow to connect
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://ecommerce-frontend-nine-sage.vercel.app"
+    "https://ecommerce-backend-tawny-nine.vercel.app"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
