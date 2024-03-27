971047e61e6830d63687ea1c376d948c1f5328ea
Matiur847 <107800262+Matiur847@users.noreply.github.com>
2024-03-27 14:56:46 +0600

    added

--- app.js
+++ app.js
@@ -14,12 +14,7 @@ const cookieParser = require("cookie-parser");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://ecommerce-backend-sable-seven.vercel.app",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(cookieParser());
 app.use(fileUpload());
