1051947d6a312ae6dab75e55a0d37d983df46bd0
Matiur847 <107800262+Matiur847@users.noreply.github.com>
2024-03-27 14:53:59 +0600

    added cors block policy

--- app.js
+++ app.js
@@ -30,11 +30,10 @@ app.use("/api/v1", orderRoute);
 app.use("/api/v1", payment);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
 
