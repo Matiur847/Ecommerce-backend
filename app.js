ae0eae5b07633319fef485d0b2545196d103fe69
Matiur847 <107800262+Matiur847@users.noreply.github.com>
2024-03-27 15:25:02 +0600

    added value

--- app.js
+++ app.js
@@ -14,12 +14,7 @@ const cookieParser = require("cookie-parser");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://ecommerce-backend-sable-seven.vercel.app",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(cookieParser());
 app.use(fileUpload());
@@ -28,27 +23,6 @@ app.use("/api/v1", productRoute);
 app.use("/api/v1", userRoute);
 app.use("/api/v1", orderRoute);
 app.use("/api/v1", payment);
-
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "POST, PUT, OPTIONS, DELETE, GET"
-  );
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://ecommerce-backend-sable-seven.vercel.app"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send("Route Not Found!");
-});
 
 // error handler
 
