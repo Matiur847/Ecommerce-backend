ebaf44f168fdf9192551e4fc3df7346d148e059b
Matiur847 <107800262+Matiur847@users.noreply.github.com>
2024-03-27 23:52:01 +0600

    added changes

--- utils/createUserJwt.js
+++ utils/createUserJwt.js
@@ -12,8 +12,6 @@ const jwtUser = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 4 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
